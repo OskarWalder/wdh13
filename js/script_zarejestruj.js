8f17cc50b33fd04fae66d2edd67bdc8b98d08aea
Antoni Nikolajski <antoni.nikolajski@gmail.com>
2025-03-20 15:14:36 +0100

    Added register page with html, css and javascript files

---
+++ js/script_zarejestruj.js
@@ -0,0 +1,12 @@
+let nazwaUzytkownika = document.getElementById("nazwa-uzytkownika");
+let email = document.getElementById("email");
+let haslo = document.getElementById("haslo");
+let pHaslo = document.getElementById("p-haslo");
+let potwierdzenie = document.getElementById("potwierdzenie");
+let ostrzezenie = document.getElementById("ostrzezenie");
+
+let zarejestrujBtn = document.getElementById("zarejestrujBtn");
+
+function error(){
+    
+}
