787b5469f4aedc8c957fce4e2ff6d54f7d531493
OskarWalder <oskarwalder37@gmail.com>
2025-03-25 00:14:08 +0100

    Working on sending emails trough contact form

--- php/database.php
+++ php/database.php
@@ -3,7 +3,7 @@
     $db_server = "localhost";
     $db_user = "root";
     $db_pass = "";
-    $db_name = "13wdh";
+    $db_name = "wdh13";
     $conn = "";
 
     $conn = mysqli_connect($db_server,  $db_user, $db_pass, $db_name);
--- php/formularz_kontaktowy.php
+++ php/formularz_kontaktowy.php
@@ -1,24 +1,62 @@
 <?php
 
     include("../php/database.php");
+    // try{
+    //     if($conn){
+            $email = $_POST["email"]; //hello@demomailtrap.co
+            $fullname = $_POST["fullname"];
+            $title = $_POST["title"];
+            $message = $_POST["message"];
+            $zgoda = $_POST["zgoda"];
 
-    $email = $_POST["email"];
-    $fullname = $_POST["fullname"];
-    $title = $_POST["title"];
-    $message = $_POST["message"];
-    $zgoda = $_POST["zgoda"];
+            // $email = $_POST("email");
+            // $fullname = $_POST("fullname");
+            // $title = $_POST("title");
+            // $message = $_POST("message");
+            // $zgoda = $_POST("zgoda");
 
+            // $sql = "INSERT INTO formularze_kontaktowe (email, imie_nazwisko, temat, wiadomosc, zgoda) VALUES ('$email' , '$fullname' , '$title' , '$message' , $zgoda')"; //trzeba jeszcze tabele do tego zrobić w bazie
 
-    // $sql = "INSERT INTO formularze_kontaktowe (email, imie_nazwisko, temat, wiadomosc, zgoda) VALUES ('$email' , '$fullname' , '$title' , '$message' , $zgoda')"; //trzeba jeszcze tabele do tego zrobić w bazie
+            // $zapytanie = mysqli_query($conn, $sql);
 
-    // $zapytanie = mysqli_query($conn, $sql);
+            // if (($zapytanie) == true) {
+            //     echo "Email został dodany do bazy";
+            // }
+            // else {
+            //     echo "Błąd.";
+            // }
 
-    // if (($zapytanie) == true) {
-    //     echo "Użytkownik został dodany do bazy";
+            use PHPMailer\PHPMailer\PHPMailer;
+            use PHPMailer\PHPMailer\SMTP;
+            use PHPMailer\PHPMailer\Exception;
+
+            require "vendor/autoload.php";
+
+            // use PHPMailer\PHPMailer\PHPMailer;
+            // use PHPMailer\PHPMailer\SMTP;
+
+            $mail = new PHPMailer(true);
+            $mail->isSMTP();
+            $mail->Host = 'live.smtp.mailtrap.io';
+            $mail->SMTPAuth = true;
+            $mail->Port = 587;
+            $mail->Username = 'api';
+            $mail->Password = 'dc199779e3b9efe54e929dd53cb94b39';
+
+            $mail->setFrom($email, $fullname);
+            $mail->addAddress("13.wejherowska.druzyna.harcerska@gmail.com", "13 Wejherowska Drużyna Harcerska im. bł. Alicji Kotowskiej");
+
+            $mail->Subject = $title;
+            $mail->Body = $message;
+
+            $mail->send();
+
+            header("Location: sent.html");
+        // }
+        mysqli_close($conn);
     // }
-    // else {
-    //     echo "Błąd.";
+    // catch(mysqli_sql_exception){
+    //     echo "Błąd połączenia z bazą danych";
     // }
 
-    mysqli_close($conn);
 ?>
