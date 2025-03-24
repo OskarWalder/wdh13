24625f31706c00628b1f9c5cd4a45f4150ac7a78
OskarWalder <oskarwalder37@gmail.com>
2025-03-24 13:09:50 +0100

    Fixing some bugs and css

--- js/script_main.js
+++
@@ -1,7 +0,0 @@
-function zaloguj(){
-    window.open("/html/zaloguj.html",this,"width=500, height=400");
-}
-
-function zarejestruj(){
-    window.open("/html/zarejestruj.html",this,"width=700, height=700");
-}
---
+++ js/script_navbar.js
@@ -0,0 +1,7 @@
+function zaloguj(){
+    window.open("../html/zaloguj.html",this,"width=500, height=400");
+}
+
+function zarejestruj(){
+    window.open("../html/zarejestruj.html",this,"width=700, height=700");
+}
